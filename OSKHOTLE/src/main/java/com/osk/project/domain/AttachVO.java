5c473a6a49542c25055687c7fabc6ba0b529ebf1
sdedu <sdedu@DESKTOP-7510KTC>
2024-05-02 14:32:12 +0900

    test

--- OSKHOTLE/src/main/java/com/osk/project/domain/AttachVO.java
+++ OSKHOTLE/src/main/java/com/osk/project/domain/AttachVO.java
@@ -16,7 +16,7 @@ import lombok.ToString;
 public class AttachVO {
    private int attachId;
    private int boardId;
-   private int attachPath;
+   private String attachPath;
    private String attachRealName;
    private String attachChgName;
    private String attachExtension;
