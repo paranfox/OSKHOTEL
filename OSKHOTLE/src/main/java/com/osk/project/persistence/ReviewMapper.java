25220cb532b951bb5806c607f960badfa2d76649
user <user@DESKTOP-DB8SKA9>
2024-05-08 09:18:12 +0900

    05.08 최정현

--- OSKHOTLE/src/main/java/com/osk/project/persistence/ReviewMapper.java
+++ OSKHOTLE/src/main/java/com/osk/project/persistence/ReviewMapper.java
@@ -2,8 +2,11 @@ package com.osk.project.persistence;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.osk.project.domain.ReviewVO;
 
+@Mapper
 public interface ReviewMapper {
 	int insert(ReviewVO reviewVO);
 	List<ReviewVO> selectListByLodging(String reviewLodgingType, int reviewLodgingNo);
